62e10b8cc4d75918061e88ff405a51d5a811b50f
camehu <camehu@gmail.com>
2023-04-24 12:30:49 -0300

    Criacao do projeto base frontend

---
+++ frontend/analizador-gastos/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1>Criação de Projeto Base - Esqueleto</h1>
+       
+      </div>
+    </>
+  )
+}
+
+export default App
